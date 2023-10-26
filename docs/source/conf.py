5379874099a705cfbf00639ce09af9b17a741976
Rafael Giebisch <rafael@Mini-von-Rafael.fritz.box>
2023-10-26 15:33:53 +0200

    Github Actions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,8 @@
 #
 import datetime
 import os
+import sys
+sys.path.insert(0, os.path.abspath('../tiktokscraper'))
 
 # -- Project information -----------------------------------------------------
 
