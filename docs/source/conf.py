a2d8130cd1be194acbb9e1267d1d7b53cb60b47b
Rafael Giebisch <rafael@Mini-von-Rafael.fritz.box>
2023-10-26 15:31:17 +0200

    Upgrade Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,6 @@
 #
 import datetime
 import os
-import sys
-sys.path.insert(0, os.path.abspath('../tiktokscraper'))
 
 # -- Project information -----------------------------------------------------
 
