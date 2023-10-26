8d5c190c0a424696bb8f4f64d6a60979973cfd9b
Rafael Giebisch <rafael@Mini-von-Rafael.fritz.box>
2023-10-26 15:42:38 +0200

    github Actions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@
 import datetime
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../tiktokscraper'))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 # -- Project information -----------------------------------------------------
 
